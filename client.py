2d54d7a6a6ff591cd175fded751ded9e5de53f9f
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-10-07 09:23:31 +0700

    Modified client code to set in while loop

--- client.py
+++ client.py
@@ -1,7 +1,17 @@
 from pwn import *
 
 s = remote("localhost", 1234)
-s.send(b"hello\n")
-print(s.recvline())
+# s.send(b"hello\n")
+# print(s.recvline())
+# s.close()
+
+# Receive the greeting
+print(s.recvuntil(b"Your name> "))
+s.send(b"horizon\n")
+
+while True:
+    line = s.recvuntil(b"> ", timeout=5)
+    print(line.decode(), end="")
+
 s.close()
 
