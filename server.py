e0217c284213cd2d8dce693e90e9a6e36b988395
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-10-07 13:06:12 +0700

    Fix typo

--- server.py
+++ server.py
@@ -84,7 +84,7 @@ def handle_client(conn):
     conn.close()
 
 def main():
-    golbal is_server_active
+    global is_server_active
     if len(sys.argv) != 2:
         print(f"Usage: python3 {sys.argv[0]} <port>")
         sys.exit(1)
